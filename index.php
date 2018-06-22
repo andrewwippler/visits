891788d9ce482f10fae478db21b3cea4ff88cb51
Andrew J Wippler <andrew.wippler@gmail.com>
2018-06-22 07:53:51 -0500

    Church Bus visits

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>LBC Soulwinning Call Generator</title>
+	<title>NSBC Soulwinning Call Generator</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
   <link href="css/style.css" rel="stylesheet" media="screen">
-</head>	
+</head>
 <body>
   <div id="wrap">
     <div class="container">
       <div class="page-header">
-        <h1>LBC Soulwinning Call Generator</h1>
+        <h1>NSBC Soulwinning Call Generator</h1>
       </div>
       <div class="alert alert-info">
         <strong>UPDATED: </strong>All the visits will be displayed on the next page when you click "Create Maps." Just print that page and visits will print one per page. Since the maps take a few seconds to generate make sure to scroll all the way to the bottom to ensure all the maps have generated.
@@ -24,15 +24,15 @@
       Note: File must be tab-delimited text, without a header row, <br />
       and contain the following fields (in the exact order listed below.)
       </p>
-      <ul> 
-        <li>First Name</li> 
-        <li>Last Name</li> 
-        <li>Main/Home Phone</li> 
-        <li>Visitor/Member</li> 
-        <li>Class/Group</li> 
-        <li>Last Attended Date</li> 
-        <li>Address Line 1</li> 
-        <li>Address Line 2</li> 
+      <ul>
+        <li>First Name</li>
+        <li>Last Name</li>
+        <li>Main/Home Phone</li>
+        <li>Address Line 1</li>
+        <li>City</li>
+				<li>State</li>
+				<li>Zip</li>
+
       </ul>
     </div>
     <div id="push"></div>
@@ -40,7 +40,7 @@
   <div id="footer">
     <div class="container">
       <p class="muted credit">
-        &copy; <?php echo date("Y"); ?> Lancaster Baptist Church
+        &copy; <?php echo date("Y"); ?> Northstar Baptist Church
       </p>
     </div>
   </div>
--- maps.php
+++ maps.php
@@ -1,12 +1,12 @@
 <?php
-session_start(); 
+session_start();
 ini_set("display_errors", "1");
 ERROR_REPORTING(E_ALL);
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>LBC Soulwinning Call Generator | Maps</title>
+	<title>NSBC Soulwinning Call Generator | Maps</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
   <link href="css/style.css" rel="stylesheet" media="screen">
@@ -15,18 +15,36 @@ ERROR_REPORTING(E_ALL);
   <div id="wrap">
     <div class="container">
       <?php
+      $apiKey = '';
       $visits = array();
+            // 0 = name
+            // 1 = lname
+            // 2 = phone
+            // 3 = Address1
+            // 4 = city
+            // 5 = state
+            // 6 = zip
+            // 7 = Parent's name
+            // 8 = Pickup time
+            // 9 = Neighborhood
       $myfile = fopen($_FILES['theFile']['tmp_name'], 'r');
       //$myfile = fopen('test.txt', 'r');
       while ($visit = fgetcsv($myfile, 1000, "\t")) {
-        if ($visit[6] == "") {
-          continue;
-        }
-        $curAddress = urlencode($visit[6] . ' ' . $visit[7]);
-      ?>
+          if ($visit[0] == "" || $visit[0] == stripos('first', $visit[0])) {
+              continue;
+          }
+
+          // check to see if it has full address, if not, add it in
+          if ($visit[4] != '' && $visit[5] != '' && $visit[6] != '') {
+              $curAddress = urlencode("$visit[3] $visit[4], $visit[5] $visit[6]");
+              $printAddress = "$visit[3]<br /> $visit[4], $visit[5] $visit[6]";
+          } else {
+              $curAddress = urlencode($visit[3] . ' Duluth, MN 55811');
+              $printAddress = $visit[3] . '<br />Duluth, MN 55811';
+          } ?>
       <div class="visit">
       <div id="map" style="width:800px;height:500px;">
-        <a src="http://maps.googleapis.com/maps/api/staticmap?center=<?php print $curAddress; ?>&zoom=16&size=800x500&markers=<?php print $curAddress; ?>&sensor=false"></a></div><br />
+        <a src="http://maps.googleapis.com/maps/api/staticmap?center=<?php print $curAddress; ?>&zoom=16&size=800x500&markers=<?php print $curAddress; ?>&sensor=false&key=<?php print $apiKey; ?>"></a></div><br />
       <table>
       <tr>
         <td width="150">Name:</td>
@@ -34,23 +52,11 @@ ERROR_REPORTING(E_ALL);
       </tr>
       <tr valign="top">
         <td>Address:</td>
-        <td><?php print $visit[6]; ?><br/><?php print $visit[7]; ?></td>
+        <td><?php echo $printAddress; ?></td>
       </tr>
       <tr>
         <td>Phone:</td>
         <td><?php print $visit[2]; ?></td>
-      </tr>
-      <tr>
-        <td>Type:</td>
-        <td><?php print $visit[3]; ?></td>
-      </tr>
-      <tr>
-        <td>Class/Group:</td>
-        <td><?php print $visit[4]; ?></td>
-      </tr>
-      <tr>
-        <td>Last Attend Date:</td>
-        <td><?php print $visit[5]; ?></td>
       </tr>
       <tr height="40">
         <td>&nbsp;</td>
